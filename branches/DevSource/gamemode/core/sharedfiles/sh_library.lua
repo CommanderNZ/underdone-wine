36a74f130ec8165e0b09d4c2465cbe9943e22d21
alexandre.sandros@gmail.com <alexandre.sandros@gmail.com@d9bc6894-f239-f7a4-58c8-1befeb0e4824>
2011-08-27 22:10:05 +0000

    clean 16 files

--- branches/DevSource/gamemode/core/sharedfiles/sh_library.lua
+++ branches/DevSource/gamemode/core/sharedfiles/sh_library.lua
@@ -1,31 +1,57 @@
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //  _    _           _              _                   __          ___              //
+    // | |  | |         | |            | |                  \ \        / (_)             //
+    // | |  | |_ __   __| | ___ _ __ __| | ___  _ __   ___   \ \  /\  / / _ _ __   ___   //
+    // | |  | | '_ \ / _` |/ _ \ '__/ _` |/ _ \| '_ \ / _ \   \ \/  \/ / | | '_ \ / _ \  //
+    // | |__| | | | | (_| |  __/ | | (_| | (_) | | | |  __/    \  /\  /  | | | | |  __/  //
+    //  \____/|_| |_|\__,_|\___|_|  \__,_|\___/|_| |_|\___|     \/  \/   |_|_| |_|\___|  //
+    //                                                                                   //
+    //       Last edit : Sandros	                                                     //
+    //                                                                                   //
+    ///////////////////////////////////////////////////////////////////////////////////////
+
 local Player = FindMetaTable("Player")
 
 function Player:AddBookToLibrary(strBook)
+
 	if !ValidEntity(self) then return false end
+	
 	self.Data.Library = self.Data.Library or {}
 	self.Data.Library[strBook] = true
+	
 	for _, strRecipe in pairs(ItemTable(strBook).GainRecipes or {}) do
+	
 		self:AddRecipe(strRecipe)
 	end
+	
 	if SERVER then
+	
 		if ItemTable(strBook).LibraryLoad then
+		
 			ItemTable(strBook):LibraryLoad(self, ItemTable(strBook))
 		end
 		SendUsrMsg("UD_UpdateLibrary", self, {strBook})
 	end
+	
 	return true
 end
 
 function Player:HasReadBook(strBook)
+
 	if !ValidEntity(self) then return false end
+	
 	return (self.Data.Library or {})[strBook]
 end
 
 if SERVER then
+
 	function Player:RequestBookStory(strBook)
+	
 		if self:HasReadBook(strBook) then
+		
 			SendUsrMsg("UD_UpdateCurrentBook", self, {true})
 			for i = 0, math.ceil(string.len(ItemTable(strBook).Story) / 150) - 1 do
+			
 				SendUsrMsg("UD_UpdateCurrentBook", self, {false, string.sub(ItemTable(strBook).Story, (i * 150) + 1, (i + 1) * 150)})
 			end
 		end
@@ -34,19 +60,29 @@ if SERVER then
 end
 
 if CLIENT then
+
 	usermessage.Hook("UD_UpdateLibrary", function(usrMsg)
+	
 		LocalPlayer():AddBookToLibrary(usrMsg:ReadString())
 	end)
+	
 	usermessage.Hook("UD_UpdateCurrentBook", function(usrMsg)
+	
 		LocalPlayer().CurrentStory = LocalPlayer().CurrentStory or ""
+		
 		if !usrMsg:ReadBool() then
+		
 			LocalPlayer().CurrentStory = LocalPlayer().CurrentStory .. usrMsg:ReadString()
 		else
+		
 			LocalPlayer().CurrentStory = ""
 		end
+		
 		if GAMEMODE.ReadMenu then
+		
 			GAMEMODE.ReadMenu:UpdateStory()
 		else
+		
 			RunConsoleCommand("UD_OpenReadMenu")
 		end
 	end)
--- branches/DevSource/gamemode/core/sharedfiles/sh_progressbars.lua
+++ branches/DevSource/gamemode/core/sharedfiles/sh_progressbars.lua
@@ -1,4 +1,17 @@
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //  _    _           _              _                   __          ___              //
+    // | |  | |         | |            | |                  \ \        / (_)             //
+    // | |  | |_ __   __| | ___ _ __ __| | ___  _ __   ___   \ \  /\  / / _ _ __   ___   //
+    // | |  | | '_ \ / _` |/ _ \ '__/ _` |/ _ \| '_ \ / _ \   \ \/  \/ / | | '_ \ / _ \  //
+    // | |__| | | | | (_| |  __/ | | (_| | (_) | | | |  __/    \  /\  /  | | | | |  __/  //
+    //  \____/|_| |_|\__,_|\___|_|  \__,_|\___/|_| |_|\___|     \/  \/   |_|_| |_|\___|  //
+    //                                                                                   //
+    //       Last edit : Sandros	                                                     //
+    //                                                                                   //
+    ///////////////////////////////////////////////////////////////////////////////////////
+	
 if CLIENT then
+
 	local tblProgressBars = {}
 	local intBarWidth = 200
 	local intBarHieght = 20
@@ -6,9 +19,13 @@ if CLIENT then
 	local intStartingY = ScrH() - intBarHieght - 100
 	local intSpacing = 5
 	
+	
 	local function DrawProgressBars()
+	
 		local intRunningHieght = intStartingY
+		
 		for strNocification, tblTimeTable in pairs(tblProgressBars or {}) do
+		
 			local tblProgressBar = jdraw.NewProgressBar()
 			tblProgressBar:SetDemensions(intStartingX, intRunningHieght, intBarWidth, intBarHieght)
 			tblProgressBar:SetStyle(4, clrGreen)
@@ -17,24 +34,35 @@ if CLIENT then
 			tblProgressBar:SetText("UiBold", strNocification, clrDrakGray)
 			jdraw.DrawProgressBar(tblProgressBar)
 			intRunningHieght = intRunningHieght - intBarHieght - intSpacing
+			
 		end
 	end
+	
 	hook.Add("HUDPaint", "DrawProgressBars", DrawProgressBars)
 
 	function AddProgressBar(strNotification, intTime)
+	
 		tblProgressBars[strNotification] = {Starting = intTime, Running = intTime}
+		
 		for i = 1, intBarWidth do
+		
 			timer.Simple(i * (intTime / intBarWidth), function() tblProgressBars[strNotification].Running = tblProgressBars[strNotification].Running - (intTime / intBarWidth) end)
 		end
+		
 		timer.Simple(intTime, function() tblProgressBars[strNotification] = nil end)
 	end
+	
 	concommand.Add("UD_AddProgressBar", function(ply, command, args) AddProgressBar(string.gsub(args[1], "_", " "), tonumber(args[2])) end)
 end
 
 if SERVER then
+
 	local Player = FindMetaTable("Player")
+	
 	function Player:CreateProgressBar(strMessage, intTime)
+	
 		if ValidEntity(self) then
+		
 			self:ConCommand("UD_AddProgressBar " .. string.gsub(strMessage, " ", "_") .. " " .. intTime) 
 		end
 	end
