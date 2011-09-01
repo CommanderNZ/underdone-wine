671f803869e4dbe442e4ef76cb152f688a5dba89
alexandre.sandros@gmail.com <alexandre.sandros@gmail.com@d9bc6894-f239-f7a4-58c8-1befeb0e4824>
2011-09-01 11:37:32 +0000

    modif player modif pvp

--- gamemode/core/serverfiles/sv_pvp.lua
+++ gamemode/core/serverfiles/sv_pvp.lua
@@ -8,42 +8,45 @@
     //                                                                                   //
     //       Last edit : Sandros Batman06                                                //
     //                                                                                   //
-    ///////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////// 
 	
 	
 
 --[[
+	
+		if !entVictim.Data.Challenge or !entAttacker.Data.Challenge then  tblDamageInfo:SetDamage(0) return end
 
+		if entVictim.Data.Challenge.Enemy != entAttacker.Data.Challenge.Enemy then tblDamageInfo:SetDamage(0) return  -- Il ne sont pas en défi
+
+		else
+			if entVictim.Data.Challenge.Time != entAttacker.Data.Challenge.Time then 
+			
+				 entVictim.Data.Challenge.Time = entAttacker.Data.Challenge.Time  -- Syncronisation des date au cas il y a un décalage 
+				 
+			end
+			
+		end 
+
+		if os.time - entVictim.Data.Challenge.Time > 600 then tblDamageInfo:SetDamage(0) return  end
+
+		if entVictim.Data.Challenge.Finish == 1 then tblDamageInfo:SetDamage(0) return 
+
+		else
+
+			if entVictim.Data.Challenge.Finish != entAttacker.Data.Challenge.Finish then
+			
+				 tblDamageInfo:SetDamage(0) return -- Utilisation de fail ! Exemple défier une personne puis une autre enfin a voir ! 
+				 
+			end
+			
+		end
+		
+		getAdjustDamage(entVictim,entInflictor)
 
 
 ]]--
 
 
-function GM:PlayerDeath(victim, weapon, killer)
-		
-	victim:Freeze(true)
-	timer.Simple(10, function()
-		
-		if ValidEntity(victim) then
-			
-			victim:Freeze(false) 
-			victim:ConCommand("+attack")
-			timer.Simple(0.1, function() if ValidEntity(victim) then victim:ConCommand("-attack") end end)
-		
-		end
-		
-	end)
-	
-	if killer:IsNPC() && victim:IsPlayer() then
-		
-		if killer.Race == victim.Race then
-			
-			killer:AddEntityRelationship(victim, GAMEMODE.RelationLike, 99)
-		end
-		
-	end
-		
-end
 
 local function getAdujstDamage( entVictim, tblDamageInfo )
 
@@ -72,77 +75,3 @@ local function getAdujstDamage( entVictim, tblDamageInfo )
 	end
 
 end
-	
-local function PlayerAdjustDamage(entVictim, entInflictor, entAttacker, intAmount, tblDamageInfo)
-	
-	if !entVictim:IsPlayer() then tblDamageInfo:SetDamage(0)  end
-	
-	if !entAttacker:IsPlayer() && entAttacker:GetOwner():IsPlayer() then
-		
-		entAttacker = entAttacker:GetOwner()
-		
-	end
-	
-	local clrDisplayColor = "red"
-	local tblNPCTable = NPCTable(entAttacker:GetNWString("npc"))
-	
-	if entAttacker:IsPlayer() then
-	
-		if !entVictim.Data.Challenge and !entAttacker.Data.Challenge then  tblDamageInfo:SetDamage(0) return end
-
-		if entVictim.Data.Challenge.Enemy != entAttacker.Data.Challenge.Enemy then tblDamageInfo:SetDamage(0) return  -- Il ne sont pas en défi
-
-		else
-			if entVictim.Data.Challenge.Time != entAttacker.Data.Challenge.Time then 
-			
-				 entVictim.Data.Challenge.Time = entAttacker.Data.Challenge.Time  -- Syncronisation des date au cas il y a un décalage 
-				 
-			end
-			
-		end 
-
-		if os.time - entVictim.Data.Challenge.Time > 600 then tblDamageInfo:SetDamage(0) return  end
-
-		if entVictim.Data.Challenge.Finish == 1 then tblDamageInfo:SetDamage(0) return 
-
-		else
-
-			if entVictim.Data.Challenge.Finish != entAttacker.Data.Challenge.Finish then
-			
-				 tblDamageInfo:SetDamage(0) return -- Utilisation de fail ! Exemple défier une personne puis une autre enfin a voir ! 
-				 
-			end
-			
-		end
-		
-		getAdjustDamage(entVictim,entInflictor)
-	end
-	
-	if tblNPCTable then
-		
-		for strNPC,_ in pairs(GAMEMODE.DataBase.NPCs or {}) do
-			
-			local tblNPCTable = NPCTable(strNPC)
-			
-			if tblNPCTable.DamageCallBack && entVictim && entAttacker.Name && entAttacker.Name ==  tblNPCTable.Name then
-				
-				tblNPCTable:DamageCallBack(entAttacker, entVictim)
-			end
-		end
-		
-		tblDamageInfo:SetDamage((tblNPCTable.DamagePerLevel or 0) * entAttacker:GetNWInt("level"))
-		tblDamageInfo:SetDamage(tblDamageInfo:GetDamage() * (1 / (((entVictim:GetArmorRating() - 1) / 10) + 1)))
-		tblDamageInfo:SetDamage(math.Clamp(math.Round(tblDamageInfo:GetDamage() + math.random(-1, 1)), 0, 9999))
-		
-		if tblNPCTable.Race == "human" then tblDamageInfo:SetDamage(0) end
-		
-		if tblDamageInfo:GetDamage() > 0 then
-			
-			entVictim:CreateIndacator(tblDamageInfo:GetDamage(), tblDamageInfo:GetDamagePosition(), clrDisplayColor)
-		else
-			
-			entVictim:CreateIndacator("Miss!", tblDamageInfo:GetDamagePosition(), "orange")
-		end
-	end		
-end
-hook.Add("EntityTakeDamage", "PlayerAdjustDamage", PlayerAdjustDamage)
